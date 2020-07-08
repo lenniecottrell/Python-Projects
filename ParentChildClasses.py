151f8ca6583309aa75649b2bd1648a6abc4374e2
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2020-07-08 11:27:10 -0700

    Create ParentChildClasses.py

---
+++ ParentChildClasses.py
@@ -0,0 +1,15 @@
+class food:
+    name = "Name"
+    perishable = True
+    mealTime = "e.g. Breakfast"
+
+class veggies(food):
+    partOfPlant = 'leaf'
+    price = 2.50
+
+
+class meats(food):
+    cut = 'filet'
+    animal = 'salmon'
+    price = 6.00
+    
