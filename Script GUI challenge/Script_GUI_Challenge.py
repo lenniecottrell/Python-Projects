996272cfdeb177cacafb18f0cd31b16bcb626efa
Lennie <64614859+lenniecottrell@users.noreply.github.com>
2020-07-15 19:52:06 -0700

    File Transfer part 2 fix

--- Script GUI challenge/Script_GUI_Challenge.py
+++ Script GUI challenge/Script_GUI_Challenge.py
@@ -1,21 +1,37 @@
 import tkinter as tk
 from tkinter import *
 
-win = tk.Tk()
-win.title("Check files")
-win.geometry("{}x{}".format(520,150))
-b1 = Button(win, width=12, text="Browse...")
-b2 = Button(win, width=12, text="Browse...")
-b3 = Button(win, width=12, height=2, text="Check for files...")
-b4 = Button(win, height=2, text="Close Program")
-b1.grid(row=1, column=0, padx=(15,0), pady=(35,0), sticky=W)
-b2.grid(row=2, column=0, padx=(15,0), pady=(10,0), sticky=W)
-b3.grid(row=3, column=0, padx=(15,0), pady=(10,10), sticky=W)
-b4.grid(row=3, column=1, padx=(10,0), pady=(10,10), sticky=E)
+import Script_GUI_Challenge_func
 
-browse1 = Entry(win, width=50, text='')
-browse1.grid(row=0, column=1, rowspan=3, padx=(15,0), pady=(10,10), sticky=E)
 
-browse2 = Entry(win, width=50,  text='')
-browse2.grid(row=1, column=1, rowspan=3, padx=(15,0), pady=(10,10), sticky=E)
+class ParentWindow(Frame):
+    def __init__ (self, *args, **kwargs):
+        Frame.__init__(self, *args, **kwargs)
+
+
+        win.title("Check files")
+        win.geometry("{}x{}".format(520,150))
+
+        self.b1 = tk.Button(win, width=12, text="Source...", command=lambda: Script_GUI_Challenge_func.selectFolder1(self))
+        self.b2 = tk.Button(win, width=12, text="Destination...", command=lambda: Script_GUI_Challenge_func.selectFolder2(self))
+        self.b3 = tk.Button(win, width=12, height=2, text="Copy Files", command=lambda: Script_GUI_Challenge_func.copyFiles(self))
+        self.b4 = tk.Button(win, height=2, text="Close Program", command=lambda: Script_GUI_Challenge_func.endProgram(self))
+
+        self.b1.grid(row=1, column=0, padx=(15,0), pady=(35,0), sticky=W)
+        self.b2.grid(row=2, column=0, padx=(15,0), pady=(10,0), sticky=W)
+        self.b3.grid(row=3, column=0, padx=(15,0), pady=(10,10), sticky=W)
+        self.b4.grid(row=3, column=1, padx=(10,0), pady=(10,10), sticky=E)
+
+        self.browse1 = tk.Entry(win, width=50, text='')
+        self.browse1.grid(row=0, column=1, rowspan=3, padx=(15,0), pady=(10,10), sticky=E)
+
+        self.browse2 = tk.Entry(win, width=50,  text='')
+        self.browse2.grid(row=1, column=1, rowspan=3, padx=(15,0), pady=(10,10), sticky=E)
+
+if __name__ == "__main__":
+    win = tk.Tk() #this is the syntax to call a tkinter window, put in the variable win
+    App = ParentWindow(win) #instantiating a class of ParentWindow called "App"
+    win.mainloop() #keep the window open
+
+
 
--- Script GUI challenge/Script_GUI_Challenge_func.py
+++ Script GUI challenge/Script_GUI_Challenge_func.py
@@ -1,21 +1,53 @@
 import os
 import tkinter as tk
 from tkinter import *
+from tkinter import filedialog
+import datetime
+import time
+import shutil
+from shutil import copyfile
 
 import Script_GUI_Challenge
 
-def selectFolder():
-    #this code will call a dialog that users can use to
-    # select a directory from their system
+
+def selectFolder1(self):
+    #call a dialog that users can use to select a directory, put it in the first entry box
+    self.source = filedialog.askdirectory()
+    self.browse1.insert(END, self.source)
+
+def selectFolder2(self):
+    #call a dialog that users can use to select a directory, put it in the second entry box
+    self.destination = filedialog.askdirectory()
+    self.browse2.insert(END, self.destination)
 
 
+def copyFiles(self):
+    files = os.listdir(self.source) #self.source is from the filedialog above
+    for i in files:
+        getSrcPath = os.path.join(self.source, i) #get the complete file path for the source
+        getDstPath = os.path.join(self.destination, i) #get the complete file path for the destination
+        modTime = os.path.getmtime(getSrcPath)#check to see the modification time of each file
+        print(modTime, i) #prints the modification time and file name
+        modTime_ezformat = time.ctime(modTime) #changes the modificaiton time to an easily understood format
+        print(modTime_ezformat) #prints the easy format, just for me to check that it works
+        now = time.time()
+        print(now)
+        last24 = int(now) - 68400
+        last24_ezformat = time.ctime(last24)
+        #Since24HoursAgo = int(modTime) - 86400 #subtracts one day from time the script is run
+        #Since24HoursAgo_ezformat = time.ctime(Since24HoursAgo) #changes the 24 hrs ago to an easy format
+        #print(Since24HoursAgo) #prints the time from 24 hrs ago
+        #print(Since24HoursAgo_ezformat) #prints the easy format from 24 hrs ago, for me to check that it works
+        
+        if int(modTime) > int(last24):
+            #copy the files represented by 'i' to the destination
+            shutil.copyfile(getSrcPath, getDstPath)
+        else:
+            pass
 
-def showFilePath():
-    #this code will show the filepath of the selected file
 
-
-
-
+def endProgram(self):
+        self.master.destroy()
 
 
 if __name__ == "__main__":
